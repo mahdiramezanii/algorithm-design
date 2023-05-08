ac392d87def5f01ac353c656efcbf917f93b9cf5
mahdiramazani <mahdiramazani128@gmail.com>
2023-05-09 01:26:51 +0430

    knpasack

--- knapsack.py
+++ knapsack.py
@@ -1,5 +1,27 @@
 import numpy
 n=4
 m=8
+weight=[0,2,3,4,5]
+value=[0,1,2,5,6]
 
-array=numpy.random.randint(0,1,size=(n,m))
+array=numpy.random.randint(0,1,size=(n+1,m+1))
+
+for i in range(n+1):
+
+    for j in range(m+1):
+
+        if ((i==0) or (j==0)):
+
+            array[i][j]=0
+
+        elif (weight[i] <= j ):
+
+            array[i][j]=max(value[i]+array[i-1][j-weight[i]] , array[i-1][j])
+
+        else:
+
+            array[i][j]=array[i-1][j]
+
+
+
+print(array[n][m])
